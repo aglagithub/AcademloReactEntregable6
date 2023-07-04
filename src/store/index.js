243fb69c98332401c917eae265db5d5c0e6e4767
Andres Lara <angillama@gmail.com>
2023-07-04 07:32:40 -0500

    Fin dia 2

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 //import userInfo from "./slices/userInfo.slice.js";
 
+
 export default configureStore({
 reducer:{
     //Aqui es donde agregamos los estados globales
